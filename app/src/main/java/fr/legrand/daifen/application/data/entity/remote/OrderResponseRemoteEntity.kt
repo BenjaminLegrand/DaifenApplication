55abd3e8d6b36ed95750690e1aa391e24a925ec7
Benjamin Legrand <blegrand@amiltone.fr>
2019-07-18 13:43:58 +0200

    Added more orders scraping

--- app/src/main/java/fr/legrand/daifen/application/data/entity/remote/OrderResponseRemoteEntity.kt
+++ app/src/main/java/fr/legrand/daifen/application/data/entity/remote/OrderResponseRemoteEntity.kt
@@ -15,18 +15,50 @@ data class OrderResponseRemoteEntity(
     var troopsDisabledWithKnowledgeWithoutBuildings: String? = null,
     @Selector("#contenu div > h2:nth-child(1) + ul + p:not(#connaissance)")
     var troopsDisabledWithoutKnowledgeWithBuildings: String? = null,
-    @Selector("#contenu div > h2:nth-child(1) + p:not(#connaissance) + p:not(#connaissance)")
+    @Selector("#contenu div > h2:nth-of-type(1) + p:not(#connaissance) + p:not(#connaissance)")
     var troopsDisabledWithoutKnowledgeWithoutBuildings: String? = null,
-    @Selector("#contenu div > h2:nth-child(1 )+ #connaissance + ul li")
+    @Selector("#contenu div > h2:nth-of-type(1) + #connaissance + ul li")
     var buildingsWithKnowledge: List<String> = emptyList(),
-    @Selector("#contenu div > h2:nth-child(1) + ul li")
+    @Selector("#contenu div > h2:nth-of-type(1) + ul li")
     var buildingsWithoutKnowledge: List<String> = emptyList(),
-    @Selector("#contenu div > h2:nth-child(1 )+ #connaissance + p:not(#connaissance) + ul li")
+    @Selector("#contenu div > h2:nth-of-type(1) + #connaissance + p:not(#connaissance) + ul li")
     var troopsWithKnowledgeWithoutBuilding: List<String> = emptyList(),
-    @Selector("#contenu div > h2:nth-child(1) + #connaissance + ul + ul li")
+    @Selector("#contenu div > h2:nth-of-type(1) + #connaissance + ul + ul li")
     var troopsWithKnowledgeWithBuildings: List<String> = emptyList(),
-    @Selector("#contenu div > h2:nth-child(1 )+ p:not(#connaissance) + ul li")
+    @Selector("#contenu div > h2:nth-of-type(1) + p:not(#connaissance) + ul li")
     var troopsWithoutKnowledgeWithoutBuilding: List<String> = emptyList(),
-    @Selector("#contenu div > h2:nth-child(1) + ul + ul li")
-    var troopsWithoutKnowledgeWithBuildings: List<String> = emptyList()
+    @Selector("#contenu div > h2:nth-of-type(1) + ul + ul li")
+    var troopsWithoutKnowledgeWithBuildings: List<String> = emptyList(),
+    @Selector("#contenu div > h2:nth-of-type(2) + p")
+    var giftsDisabled: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(2) + ul li")
+    var gifts: List<String> = emptyList(),
+    @Selector("#contenu div > h2:nth-of-type(3) + p")
+    var specialTroopsDisabled: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(3) + ul li")
+    var specialTroops: List<String> = emptyList(),
+    @Selector("#contenu div > h2:nth-of-type(4) + p")
+    var firstAttackTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(4) + p + ul li")
+    var firstAttack: List<String>? = null,
+    @Selector("#contenu div > h2:nth-of-type(4) + p + ul + p")
+    var secondAttackTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(4) + p + ul + p + ul li")
+    var secondAttack: List<String>? = null,
+    @Selector("#contenu div > h2:nth-of-type(4) + p + ul + p + ul + p")
+    var thirdAttackTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(4) + p + ul + p + ul + p + ul li")
+    var thirdAttack: List<String>? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p")
+    var firstSupportTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p + ul li")
+    var firstSupport: List<String>? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p + ul + p")
+    var secondSupportTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p + ul + p + ul li")
+    var secondSupport: List<String>? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p + ul + p + ul + p")
+    var thirdSupportTarget: String? = null,
+    @Selector("#contenu div > h2:nth-of-type(5) + p + ul + p + ul + p + ul li")
+    var thirdSupport: List<String>? = null
 )
